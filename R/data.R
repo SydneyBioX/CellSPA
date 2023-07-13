5e9321d1659636d3114b86eedc0285007dc5def6
yingxinlin <yingxinlindsay@gmail.com>
2023-07-13 14:39:15 -0400

    add data script

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' An example SpatialExperiment data
+#'
+#' A subset of 10x Xenium data in SpatialExperiment format
+#'
+#' @format ## `spe`
+#' A SpatialExperiment as example to illustrate CellSPA:
+#'
+
+"spe"
